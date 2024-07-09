09c983b8211f5cce48ac046315fde952e5db0200
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-09 09:25:30 -0400

    Print tests for build_tree

--- test.rb
+++ test.rb
@@ -4,7 +4,11 @@ $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
 
 require 'tree'
 
-test = Tree.new((Array.new(15) { rand(1..100) }))
+array = (Array.new(15) { rand(1..100) })
+p array
+test = Tree.new(array)
+puts test.root
+puts test.root.left_node
 
 # Confirm that the tree is balanced by calling #balanced?
 # Print out all elements in level, pre, post, and in order
