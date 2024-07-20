2832e22f2a562fe54b9dc6b3ad58731052c11a03
jeffbarsoum <143226227+jeffbarsoum@users.noreply.github.com>
2024-07-20 05:25:52 -0400

    Add delete testing

--- test.rb
+++ test.rb
@@ -4,13 +4,13 @@ $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
 
 require 'tree'
 
-array = (Array.new(15) { rand(1..100) })
+array = (Array.new(40) { rand(1..500) })
 p array
 test = Tree.new(array)
 puts test.root
 
 # puts 'test preorder:'
-# test.preorder { |leaf| p leaf }
+# test.preorder { |leaf| p leaf.value }
 # p test.preorder
 # puts 'test inorder:'
 # test.inorder { |leaf| p leaf }
@@ -18,9 +18,17 @@ puts test.root
 # puts 'test postorder:'
 # test.postorder { |leaf| p leaf }
 # p test.postorder
-puts 'test levelorder:'
-test.level_order { |leaf| p leaf }
-p test.level_order
+# puts 'test levelorder:'
+# test.level_order { |leaf| p leaf }
+# p test.level_order
+
+# p "find first item: #{test.find(array[0])}"
+# p "test finding nonexistent item: #{test.find('cat')}"
+p "delete #{array[0]}"
+
+test.delete(array[0])
+puts test.root
+# test.inorder { |leaf| p leaf if leaf.right_node.nil? || leaf.left_node.nil? }
 
 # Confirm that the tree is balanced by calling #balanced?
 # Print out all elements in level, pre, post, and in order
